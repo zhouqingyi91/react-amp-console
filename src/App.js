35effd20a920ea4d9f71f6b42bac7a90f874b89f
zqy <luffy.zhouqingyi@icloud.com>
2023-01-31 16:34:04 -0800

    added backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { API } from "aws-amplify";
+
+
+const prefix = "https://luffysfightclub.s3.us-west-2.amazonaws.com/";
 
 function App() {
+  const [IMG_DATA, setIMG_DATA] = useState(null);
+  useEffect(() => {
+    if (IMG_DATA === null) {
+      fetchImgData();
+    }
+  }, [])
+
+  const fetchImgData = async () => {
+    try {
+      const res = await API.post('getS3AlbumPhotosApi', '/s3/album', { body: { album: "portrait/" } });
+      console.log(res);
+      setIMG_DATA(res);
+    } catch (err) {
+      console.log("err", err);
+      console.log("error messsage = ", err.message);
+      console.log("error response = ", err.response);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {IMG_DATA && IMG_DATA.map((url, idx) => {
+        console.log(url);
+        return <img src={prefix + url.path} key={idx} height="200px" width={"200px"} />
+      })}
     </div>
   );
 }
